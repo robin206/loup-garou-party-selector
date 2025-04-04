123437a7d27f03f3de271b027e8528bb0874a926
Robin206 <robin206@gmail.com>
2025-04-04 15:00:17 +0200

    Revert "feat: Add help page with roles"
    This reverts commit 911bc2f5caca86c8bea426a98696cb6f296c940f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Game from './pages/Game';
-import Home from './pages/Home';
-import Settings from './pages/Settings';
-import Setup from './pages/Setup';
-import Help from './pages/Help';
-import { AppContextProvider } from './context/AppContext';
-import { Phase, Role } from './types';
-import Themes from './pages/Themes';
 
-function App() {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Index from "./pages/Index";
+import Game from "./pages/Game";
+import Rules from "./pages/Rules";
+import Config from "./pages/Config";
+import Setup from "./pages/Setup";
+import Distribution from "./pages/Distribution";
+import NotFound from "./pages/NotFound";
 
-  useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
+// Create a client outside of the component
+const queryClient = new QueryClient();
 
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/rules" element={<Rules />} />
+            <Route path="/config" element={<Config />} />
+            <Route path="/setup" element={<Setup />} />
+            <Route path="/distribution" element={<Distribution />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
 
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
-
-  return (
-    <AppContextProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/setup" element={<Setup />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/themes" element={<Themes />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-    </AppContextProvider>
-  );
-}
-
-export default App
+export default App;
